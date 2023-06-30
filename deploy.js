ad75b06912c3286344f7437513d57406ad26deb7
Shreyas <131350902+ShreyasRajiv327@users.noreply.github.com>
2023-06-30 23:26:07 +0530

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy("TokenName", "TKN", 18, ethers.utils.parseEther("1000000"));
+
+  await token.deployed();
+
+  console.log("Token address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
